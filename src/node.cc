63470700fa0c043abe14ec345647d2486d8a08c7
Carine Zhang <zh.carine@gmail.com>
2018-07-28 18:33:04 +0200

    add insert for Node (not finished)

--- src/node.cc
+++ src/node.cc
@@ -1,5 +1,38 @@
 #include "node.hh"
 
-void Node::insert(char* word, int frequency, std::string data) {
+void Node::insert(std::string word, int frequency, std::string& data) {
+	int wordLength = word.length();
+	int dataLength = data.length();
+	for (auto it = sons_.begin(); it != sons_.end(); it++) {
+		int start = it->start_;
+		int length = it->length_;
+		if (data[start] == word.at(0)) {
+			int i = 1;
+			while (data[start + i] == word.at(i)){
+				i++;
+			}
+			if (i == wordLength && length == i) {
+				it->frequency_ = frequency;
+				return;
+			}
+			if (i > length) {
+				insert(word.substr(i), frequency, data);
+				return;
+			}
 
+			//split current Node
+			Node node(start + i, length - i, it->frequency_);
+			it->length_ = i;
+			it->frequency_ = frequency;
+			it->sons_.push_back(node);
+		
+
+			if (i < wordLength) {
+				it->frequency_ = 0;
+				addSon(it->sons_, frequency, data, word.substr(i));
+				return;
+			}
+		}
+		addSon(sons_, frequency, data, word);
+	}
 }
--- src/node.hh
+++ src/node.hh
@@ -4,13 +4,15 @@
 class Node {
   public:
     Node();
+		Node(int start, int length, int frequency);
     ~Node();
-		void insert(char* word, int frequency, std::string data);
+		void insert(std::string word, int frequency, std::string& data);
+		void addSon(std::list<Node> sons, int frequency, std::string& data, std::string word):
 		void serialize();
 	private:
-		int start;
-		int length;
-		int frequency;
-		std::list<Node> sons;
+		int start_;
+		int length_;
+		int frequency_;
+		std::list<Node> sons_;
 		//char padding[7];
 };
