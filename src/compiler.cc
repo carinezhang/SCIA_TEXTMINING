031853a2102338aab85fae3521265f9f343cea89
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-31 20:38:43 +0200

    minors fix

--- src/compiler.cc
+++ src/compiler.cc
@@ -8,8 +8,6 @@
 
 int main(int argc, char **argv)
 {
-  
-  std::cout << "Size structure: 4" << std::endl;
   if (argc != 3)
   {
     std::cout << "Usage: ./TextMiningCompiler /path/to/word/freq.txt /path/to/output/dict.bin" << std::endl;
@@ -35,36 +33,16 @@ int main(int argc, char **argv)
     int freq;
     if (!(iss >> word >> freq))
       break;
-    
-    //std::cout << "Line : " << word << " " << freq << std::endl;
-    //std::cout << "data :" << trie.data_ << std::endl;
-
     trie.insert(word, freq);
   }
-  //trie.print();
 
   infile.close();
-  // Write the trie in the output file
+
   std::fstream output(output_name, std::fstream::out | std::fstream::binary);
   trie.serialize(output);
   std::cout << "nb:" << trie.nbprint() << std::endl;
   output.close();
 
-  std::cout << "===============================" << std::endl;
-  std::cout << "DESERIALIZE" << std::endl;
 
-  std::fstream test_in(output_name, std::fstream::in | std::fstream::binary);
-  Trie trie_test = Trie();
-  trie_test.deserialize(test_in);
-  //trie_test.printword(); 
-  std::cout << "nb:" << trie_test.nbprint() << std::endl;
-  
-  std::cout << "=========================" << std::endl;
-  
-  auto res = trie_test.search("bonjour", 0);
-  std::cout << "SEARCH nb:" << res.size() << std::endl;
-  res =trie_test.sort(res);
-  trie_test.printdist(res);
-  std::cout << "=========================" << std::endl;
   return 0;
 }
--- src/search.hh
+++ src/search.hh
@@ -2,18 +2,67 @@
 
 #include <string>
 
+/**
+ * @file
+ * @author drapal_g zhang_d
+ *
+ * @section DESCRIPTION
+ *
+ * The Node class represent a node of a Patricia-Trie containing in Trie class.
+ * The Search class is the result of a call to the Trie's search function.
+ * It contains all the words, their distances and frequency. 
+ *
+ */
+
 class Search
 {
   public:
+    /**
+     * Constructor with all the attributes.
+     *
+     * @param word  The word.
+     * @param freq  The frequency of the word.
+     * @param dist  The distance of this word to the word searched.
+     */
     Search(std::string word, int freq, int dist);
+
+    /**
+     * Default destructor.
+     */
     ~Search();
+
+    /**
+     * Setter for the dist attribute.
+     */
     void dist_set(int a);
+
+    /**
+     * Getter for the dist attribute.
+     */
     int dist_get();
+
+    /**
+     * The word.
+     */
     std::string word_;
+
+    /**
+     * The frequency of the word.
+     */
     int freq_;
+
+    /**
+     * The distance of this word to the searched word in the Trie's search function.
+     */
     mutable int dist_;
 };
 
+/**
+ * Operator < for Search objects.
+ *
+ * @param left  A search object.
+ * @param right A search object.
+ */
 inline bool operator< (const Search &left, const Search &right)
 {
   return left.word_ < right.word_;
