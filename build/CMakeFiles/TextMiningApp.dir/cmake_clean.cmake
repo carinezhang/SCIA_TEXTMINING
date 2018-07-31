74ff9e83a298d10c57873f1b6c27cb95bfa7b9e1
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-31 22:02:47 +0200

    Add build (sorry :/)

---
+++ build/CMakeFiles/TextMiningApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextMiningApp.dir/src/app.cc.o"
+  "CMakeFiles/TextMiningApp.dir/src/node.cc.o"
+  "CMakeFiles/TextMiningApp.dir/src/trie.cc.o"
+  "CMakeFiles/TextMiningApp.dir/src/search.cc.o"
+  "TextMiningApp.pdb"
+  "TextMiningApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextMiningApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
